b68f7cf61827f36f95bba61ab571e03f50ea2f9f
LucDuponcheelAtGitHub <luc.duponcheel@gmail.com>
2026-07-21 15:56:26 +0200

    changed coEndoYonedaEquiv formulation

--- CoEndoYoneda.lean
+++ CoEndoYoneda.lean
@@ -322,7 +322,8 @@ theorem φ_const_comp_γμ {W : C} (g : FF.obj PUnit ⟶ (GF_def FF).obj W) :
   erw [h_g_eta] at h_final2
   exact h_final2
 
-def coEndoYonedaEquiv {F : C ⥤ C} {X : C} : (CYEF X ⟶ F ⋙ GF) ≃ (FF.obj PUnit ⟶ GF.obj (F.obj X)) where
+def coEndoYonedaEquiv {F : C ⥤ C} {X : C} : (CYEF X ⟶ F ⋙ GF) ≃ (FF.obj PUnit ⟶ ((F ⋙ GF).obj X))
+where
   toFun τ := (φ (X := PUnit) (Y := X ⟶ X) (fun _ => 𝟙 X)) ≫ τ.app X
   invFun g := globalFunctorialValueToNaturalTransformation2 F g
   left_inv := fun τ ↦ (coEndoYonedaLemma2 τ).symm
